97a734f69a2553ee02fdc4966e18c347976c7812
Aleksey-Dr <aprel181818@gmail.com>
2023-02-24 23:13:27 +0200

    Video. Lesson 8 part 2. Iteration array methods
    Video. Lesson 8 part 2. Iteration array methods
    Method sort()
    sorting complex data types (objects array)

--- js/java-script.js
+++ js/java-script.js
@@ -680,3 +680,103 @@ const totalPrice = cart.reduce((total, { price, quantity }) =>
     total + price * quantity, 0);
 
 console.log(totalPrice);
+
+// Video. Lesson 8 part 2. Iteration array methods
+console.log('Video. Lesson 8 part 2. Iteratoin array methods');
+
+// Method sort()
+console.log('Method sort()');
+
+const numbersD = [1, 6, 8, 9, 4, 3, 7, 5];
+console.log('numbersD before:', numbersD);
+// numbersD.sort();
+// console.log('numbersD after:', numbersD);
+// numbersD.sort((curEl, nextEl) => nextEl - curEl);
+// console.log('numbersD doubleafter:', numbersD);
+
+const numbersE = ['A', 'D', 'B', 'F', 'C', 'E'];
+console.log('numbersE before:', numbersE);
+numbersE.sort();
+console.log('numbersE after:', numbersE);
+numbersE.sort((curEl, nextEl) => nextEl - curEl);
+console.log('numbersE doubleafter:', numbersE);
+
+const copyNumbersD = [...numbersD];
+console.log('Copy before:', copyNumbersD);
+copyNumbersD.sort();
+console.log('Copy after:', copyNumbersD);
+
+// sorting complex data types (objects array)
+console.log('Sorting complex data types');
+
+const users = [
+    {
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        eyeColor: "blue",
+        friends: ["Sharron Pace"],
+        isActive: false,
+        balance: 2811,
+        gender: "male"
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        eyeColor: "blue",
+        friends: ["Briana Decker", "Sharron Pace"],
+        isActive: true,
+        balance: 3821,
+        gender: "female"
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        eyeColor: "green",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+        isActive: false,
+        balance: 3793,
+        gender: "male"
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        eyeColor: "green",
+        friends: ["Goldie Gentry", "Aisha Tran"],
+        isActive: true,
+        balance: 2278,
+        gender: "female"
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        eyeColor: "blue",
+        friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+        isActive: true,
+        balance: 3951,
+        gender: "male"
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        eyeColor: "brown",
+        friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+        isActive: false,
+        balance: 1498,
+        gender: "male"
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        eyeColor: "brown",
+        friends: ["Goldie Gentry", "Briana Decker"],
+        isActive: true,
+        balance: 2764,
+        gender: "female"
+    }
+];
+
+console.table(users);
+const usersBalance = [...users].sort((minBalance, maxBalance) =>
+    minBalance.balance - maxBalance.balance);
+
+console.table(usersBalance);
